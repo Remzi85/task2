02ea0d2a3e862fbf500b2f3f306b1813aa5bffb5
Remzi Ahmedov <130896825+Remzi85@users.noreply.github.com>
2024-11-30 13:49:16 +0400

    Add files via upload

---
+++ task2/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace task2
+{
+    internal class Car
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+
+        public Car(string v1, string v2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car("Mercedes", "E200", "Sedan");
+            var car2 = new Car("BMW", "X5", "SUV");
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+        }
+    }
+}
